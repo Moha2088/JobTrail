321986a4be9376f34dc4a67adc638e8eaf0789e9
Mohamed Shil <maxamed-14@outlook.dk>
2026-03-05 12:07:06 +0100

    fix lint

--- jobtrail-frontend/cypress.config.ts
+++ jobtrail-frontend/cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 
     e2e: {
         setupNodeEvents(on, config) {
-      // implement node event listeners here
+        // implement node event listeners here
         },
     },
 })
