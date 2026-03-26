fb5d347824e5a665738a5021711713a5e0e0ca47
Mohamed Shil <maxamed-14@outlook.dk>
2026-03-26 12:17:38 +0100

    remove unused prompt

--- jobtrail-frontend/src/providers/openAIProvider.ts
+++ jobtrail-frontend/src/providers/openAIProvider.ts
@@ -10,7 +10,3 @@ export const createContentPrompt = (content: string) => {
     return "Rewrite and optimize the following content for a job application, in terms of grammar and in structure. " +
         "Make it more appealing and professional: " + content
 }
-
-export const createCompanyRecommendationPromp = (companies: string) => {
-    return "Based on the following list of positions, and companies that I have applied to, what other companies should I apply to which falls under the same domain and hires similar roles? Recommend up to 5 companies: " + companies
-}
