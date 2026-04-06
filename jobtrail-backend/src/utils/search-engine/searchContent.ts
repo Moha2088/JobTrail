9d21fff9c6ffe0791a0ab6b83a2a414dda0b77b9
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-06 19:46:35 +0200

    Update searchContent.ts

--- jobtrail-backend/src/utils/search-engine/searchContent.ts
+++ jobtrail-backend/src/utils/search-engine/searchContent.ts
@@ -6,7 +6,7 @@ export async function searchContent(query: string, userId: number) {
     const searchResults = await db.execute(sql`
         SELECT * 
         FROM applications
-        WHERE to_tsvector('danish', "Content") @@ to_tsquery('danish', ${query})
+        WHERE to_tsvector('danish', "Content") @@ plainto_tsquery('danish', ${query})
         AND "UserId" = ${userId}
     `)
 
