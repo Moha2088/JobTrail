643294136c5650252457b41416f62ea862ed475c
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-18 15:07:34 +0200

    update

--- jobtrail-frontend/src/app/api/apiClients.ts
+++ jobtrail-frontend/src/app/api/apiClients.ts
@@ -5,8 +5,10 @@ import axios from "axios"
 
 export const isProduction = process.env.NODE_ENV === "production"
 
-export const elysiaApi = treaty<App>(isProduction ? process.env.PROD_NEXT_BACKEND_URL! : process.env.NEXT_BACKEND_URL!)
+const url = isProduction ? process.env.PROD_NEXT_BACKEND_URL! : process.env.NEXT_BACKEND_URL!
+
+export const elysiaApi = treaty<App>(url)
 
 export const axiosClient = axios.create({
-    baseURL: isProduction ? process.env.PROD_NEXT_BACKEND_URL! : process.env.NEXT_BACKEND_URL!
+    baseURL: url
 })
