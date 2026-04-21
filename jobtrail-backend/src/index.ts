70354c2e32fecd6b5a47d06cb965ee013922e223
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-21 22:41:05 +0200

    update

--- jobtrail-backend/src/index.ts
+++ jobtrail-backend/src/index.ts
@@ -4,7 +4,6 @@ import { authRouter } from "./routes/auth/route";
 import openapi from "@elysiajs/openapi";
 import { cors } from "@elysiajs/cors"
 import { jwtConfig } from "./utils/auth/jwt";
-import { handleNotFoundError } from "./handlers/handleNotFound"
 import "./messaging/eventhandlers/applications/jobHandlers"
 import { logger } from "./logger";
 import { healthRouter } from "./routes/health/route";
