8a95b175b62515231d7878f06736edf8944611d5
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-23 10:31:12 +0200

    add log for debugging

--- jobtrail-frontend/src/app/api/apiClients.ts
+++ jobtrail-frontend/src/app/api/apiClients.ts
@@ -7,6 +7,7 @@ export const isProduction = process.env.NODE_ENV === "production"
 
 const url = isProduction ? process.env.PROD_NEXT_BACKEND_URL! : process.env.NEXT_BACKEND_URL! || "http://localhost:3003"
 
+console.log("API URL used: " + url)
 export const elysiaApi = treaty<App>(url)
 
 export const axiosClient = axios.create({
