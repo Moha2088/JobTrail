0f86cf216d8221470ad1155929b44ae7f1b42f1c
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-23 10:39:06 +0200

    change name for prod backend url

--- jobtrail-frontend/src/app/api/apiClients.ts
+++ jobtrail-frontend/src/app/api/apiClients.ts
@@ -5,7 +5,7 @@ import axios from "axios"
 
 export const isProduction = process.env.NODE_ENV === "production"
 
-const url = isProduction ? process.env.PROD_NEXT_BACKEND_URL! : process.env.NEXT_BACKEND_URL! || "http://localhost:3003"
+const url = isProduction ? process.env.NEXT_PUBLIC_PROD_BACKEND_URL! : process.env.NEXT_BACKEND_URL! || "http://localhost:3003"
 
 console.log("API URL used: " + url)
 export const elysiaApi = treaty<App>(url)
