7fb1dc948966d1508bda3f485b0658de37e71555
Mohamed Shil <maxamed-14@outlook.dk>
2026-05-09 18:03:50 +0200

    fix router

--- jobtrail-frontend/src/components/ui/view/Navbar.tsx
+++ jobtrail-frontend/src/components/ui/view/Navbar.tsx
@@ -3,20 +3,20 @@
 import { useSessionContext } from "@/contexts/session/SessionContext"
 import { IconLogout, IconUser } from "@tabler/icons-react"
 import Link from "next/link"
-import router from "next/router"
 import { Button } from "../controls/Button"
 import { useLogOut } from "@/services/auth/useLogOut"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 
 export function Navbar() {
     const pathname = usePathname()
     const session = useSessionContext()
     const logOut = useLogOut()
+    const router = useRouter()
 
     return (
         <div>
-            {session &&
+            {session && pathname != "/" &&
                 <div className="flex absolute justify-end items-center w-screen mr-10 gap-1 p-3 ">
                     {!pathname.startsWith("/user") && 
                         <div className="p-2 mr-3 border-gray-100 border-2 rounded-full hover:bg-gray-200">
