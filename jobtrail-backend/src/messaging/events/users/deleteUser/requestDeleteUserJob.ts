f109a5671fd0c46c1342839c80360f32b0fde1dc
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-02 13:53:46 +0200

    update ms delay

--- jobtrail-backend/src/messaging/events/users/deleteUser/requestDeleteUserJob.ts
+++ jobtrail-backend/src/messaging/events/users/deleteUser/requestDeleteUserJob.ts
@@ -3,7 +3,7 @@ import { usersQueue } from "../../../queue";
 
 export async function requestDeleteUserJob(userId: number, jobOptions: Partial<JobsOptions> = { }) {
     const jobId = "delete_user_" + userId
-    const delay = 86400000
+    const delay = 600000
 
     await usersQueue.add("delete_user/" + userId, {
         type: "delete_user",
