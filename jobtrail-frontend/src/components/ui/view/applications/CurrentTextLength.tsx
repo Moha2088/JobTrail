2f36b37e5bd22683d750d77934a6a08429b2026b
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-05 13:22:53 +0200

    update styling for textlength component

--- jobtrail-frontend/src/components/ui/view/applications/CurrentTextLength.tsx
+++ jobtrail-frontend/src/components/ui/view/applications/CurrentTextLength.tsx
@@ -9,7 +9,7 @@ export function CurrentTextLength(props: TextLengthProps) {
     const hasReachedLimit = currentLength >= limit
 
     return (
-        <div className={`flex gap-0.5 ${hasReachedLimit ? "text-green-300" : "text-red-400"}`}>
+        <div className={`flex gap-0.5 ${hasReachedLimit ? "text-green-400" : "text-red-400"}`}>
             <div className="mt-0.5">
                 <p className="text-xs">
                     {currentLength}
