7fd737a6dff54697cbcab528aa3b7317cee6db92
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-07 21:55:13 +0200

    update

--- jobtrail-backend/src/messaging/queue.ts
+++ jobtrail-backend/src/messaging/queue.ts
@@ -3,16 +3,14 @@ import { UserQueueJobs } from "./events/users/jobTypes"
 
 export const usersQueueName = "users"
 
-const isProduction = process.env.NODE_ENV === "production"
-
-if(isProduction && (!process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT || !process.env.RAILWAY_REDIS_PASSWORD)) {
+if(!process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT || !process.env.RAILWAY_REDIS_PASSWORD) {
     throw new Error("Redis host and password is required!")
 }
 
 export const connection: ConnectionOptions = {
-    host: isProduction ? process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT : "localhost",
+    host: process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT ? process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT : "localhost",
     port: 6379,
-    ...(isProduction ? {
+    ...(process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT ? {
         password: process.env.RAILWAY_REDIS_PASSWORD
     }
     : {})
