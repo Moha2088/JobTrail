53a903c66c7f5bb8a0131f93977c5abb5b786858
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-07 21:52:56 +0200

    update redis config

--- deletion-service/src/messaging/queue.ts
+++ deletion-service/src/messaging/queue.ts
@@ -4,29 +4,19 @@ import { UserQueueJobs } from "./events/users/jobTypes"
 
 export const usersQueueName = "users"
 
-const isProduction = process.env.NODE_ENV === "production"
 
-if(isProduction && (!process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT || !process.env.RAILWAY_REDIS_PASSWORD)) {
+if(!process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT || !process.env.RAILWAY_REDIS_PASSWORD) {
     throw new Error("Redis host and password is required!")
 }
 
 export const connection: ConnectionOptions = {
-    host: isProduction ? process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT : "localhost",
+    host: process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT ? process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT : "localhost",
     port: 6379,
-    ...(isProduction ? {
+    ...(process.env.RAILWAY_REDIS_PRIVATE_ENDPOINT ? {
         password: process.env.RAILWAY_REDIS_PASSWORD
     }
     : {})
 }
-
-export const ioredis = new Redis({
-    host: connection.host,
-    port: 6379,
-    ...(isProduction ? {
-        password: process.env.RAILWAY_REDIS_PASSWORD
-    }
-    : {})
-})
 
 
 export const usersQueue = new Queue<UserQueueJobs>(usersQueueName, {
