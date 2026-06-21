9b45376e633c9839913e692064696ca6a0b30bcf
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-21 15:05:23 +0200

    update

--- jobtrail-frontend/src/app/page.tsx
+++ jobtrail-frontend/src/app/page.tsx
@@ -27,9 +27,10 @@ export default function Home() {
         setPreviewTab(tab)
     }
 
+
     return (
         <>
-            <div className="bg-linear-to-b from-blue-300 via-blue-300 to-white min-h-screen md:h-300 md:min-h-0 pb-10 md:pb-0 mb-10">
+            <div className=" min-h-screen md:h-200 md:min-h-0  md:pb-0">
 
                 <div className="p-3" />
 
@@ -70,10 +71,10 @@ export default function Home() {
                     <motion.div
                         whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 0.9 }}
-                        className="mr-auto ml-auto w-fit">
+                        className="mr-auto ml-auto w-fit mb-10">
                         <Label
-                            iconStart={<IconSparklesFilled color="white" />}
-                            iconEnd={<IconPointFilled className="pt-1" size={20} color="white" />}
+                            iconStart={<IconSparklesFilled color="black" />}
+                            iconEnd={<IconPointFilled className="pt-1 animate-pulse" size={20} color="lightgreen" />}
                         >
                             <Link href="/login">
                                 Enhance your jobtracking now
@@ -81,10 +82,8 @@ export default function Home() {
                         </Label>
                     </motion.div>
 
-                    <div className="p-5" />
-
                     <div className="flex justify-center items-center">
-                        <p className="text-4xl font-bold text-white max-w-full md:max-w-300 px-4 text-center">
+                        <p className={"text-4xl font-bold bg-linear-to-r bg-clip-text text-transparent from-black to-purple-600 max-w-full md:max-w-300 px-4 text-center"}>
                             AI assisted job tracking for everyone
                         </p>
                     </div>
@@ -107,6 +106,27 @@ export default function Home() {
                 </motion.div>
             </div>
 
+            <div className=" mb-20">
+                <div className="flex justify-center">
+                    <p className="text-4xl font-bold">
+                        Submit an application
+                    </p>
+                </div>
+
+                <div className="flex justify-center mb-10">
+                    <p className="text-4xl font-bold text-gray-400">
+                        Optimize it instantly
+                    </p>
+                </div>
+
+                <div className="ml-auto mr-auto max-w-170">
+                    <p className="text-gray-500">
+                        Why wait until after your applications has been rejected multiple times before optimizing it? 
+                        With Jobtrail, you can create an application and immediately improve it
+                    </p>
+                </div>
+            </div>
+
             <div className="flex justify-center mb-20">
                 <PreviewTab
                     setPreviewTab={handleTab}
@@ -119,13 +139,17 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="flex justify-center items-center mb-20">
-                <Image
+            <motion.div 
+                className="flex justify-center items-center mb-20">
+                <MotionImage
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}
                     alt="" src={previewTab == "Application Management" ? "/landing/create-application-demo.gif" : "/landing/enhance-ai-demo.gif"}
                     width={1}
                     height={1}
                     className="w-200 h-100 rounded-xl"/>
-            </div>
+            </motion.div>
 
             <div className="flex flex-col justify-center items-center gap-3 mb-20">
                 <div className="flex flex-row gap-2">
@@ -136,6 +160,39 @@ export default function Home() {
                 <div className="flex flex-row gap-2">
                     <p>AI insight and recommendations</p>
                     <IconCheck color="lightblue" size={30} />
+                </div>
+            </div>
+
+            <div className="mr-auto ml-auto bg-black rounded-xl w-250 h-100">
+                <div className="flex justify-center p-10 mb-10">
+                    <p className="text-3xl text-white font-bold mt-20">
+                        Start your journey now!
+                    </p>
+                </div>
+
+                <div className="flex justify-center items-center gap-3">
+                    <div>
+                        <MotionButton
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            size="small"
+                            onClick={() => router.push("/login")}
+                        >
+                            Log in
+                        </MotionButton>
+                    </div>
+
+                    <div>
+                        <MotionButton
+                            variant="light"
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}
+                            size="small"
+                            onClick={() => router.push("/signup")}
+                        >
+                            Sign Up
+                        </MotionButton>
+                    </div>
                 </div>
             </div>
 
