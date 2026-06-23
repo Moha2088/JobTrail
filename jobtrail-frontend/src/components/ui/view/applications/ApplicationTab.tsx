f5ecd62afc34843f93db9919902a3a4005ebe451
Mohamed Shil <maxamed-14@outlook.dk>
2026-06-23 21:56:42 +0200

    update

--- jobtrail-frontend/src/components/ui/view/applications/ApplicationTab.tsx
+++ jobtrail-frontend/src/components/ui/view/applications/ApplicationTab.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useIsMobile } from "@/hooks/useIsMobile"
 import { cn } from "@/lib/utils"
 import { IconArticle, IconFlameFilled } from "@tabler/icons-react"
 import { ComponentProps } from "react"
@@ -13,12 +14,13 @@ interface ApplicationTabProps {
 export function ApplicationTab(props: ApplicationTabProps) {
     const { setActiveTab } = props
 
+    const isMobile = useIsMobile()
     
     return (
         <Tabs className="relative md:absolute mt-5 md:mt-10" defaultValue="applications">
             <TabsList variant="line">
-                <TabsTrigger className="hover:cursor-pointer" onClick={() => setActiveTab("applications")} value="applications">Applications <IconArticle /></TabsTrigger>
-                <TabsTrigger className="hover:cursor-pointer" onClick={() => setActiveTab("explore")} value="explore">Explore <IconFlameFilled color="red" /> </TabsTrigger>
+                <TabsTrigger className="hover:cursor-pointer" onClick={() => setActiveTab("applications")} value="applications">{isMobile ? "": "Applications"} <IconArticle /></TabsTrigger>
+                <TabsTrigger className="hover:cursor-pointer" onClick={() => setActiveTab("explore")} value="explore">{isMobile ? "" : "Explore"} <IconFlameFilled color="red" /> </TabsTrigger>
             </TabsList>
         </Tabs>
     )
